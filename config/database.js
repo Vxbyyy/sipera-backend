ef5b770e47e31fd7bae281eb118e32196940576f
CG <suci74141@gmail.com>
2026-06-23 14:20:08 +0800

    debug db

--- config/database.js
+++ config/database.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
 
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_PORT =", process.env.DB_PORT);
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
